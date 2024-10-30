6b5a78a11057dab2760a2d634fe820da626f9cab
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-30 02:50:01 -0300

    created cria_db function

--- db.py
+++ db.py
@@ -2,3 +2,7 @@ from sqlalchemy import create_engine#, Table, Column, String, Integer, select, t
 from utils.constants import DB_URL
 
 engine = create_engine(DB_URL, echo=False)
+Session = sessionmaker(bind=engine)
+
+def cria_db():
+    Base.metadata.create_all(bind=engine)
