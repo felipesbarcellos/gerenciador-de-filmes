81a6f984f2181968abb5e75d8a594580b1f1f4b7
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-30 02:51:55 -0300

    Added imports to main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from views.view import *
-from controllers.filmes_controller import ControllerFilmes 
-
+from controllers.filmes_controller import ControllerFilmes
+from db import cria_db
+from models import *
 
 if __name__ == "__main__":
     app = App()
