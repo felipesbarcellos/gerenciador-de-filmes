e729043e716d3223e14e574caf31238eae8a858a
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-30 02:50:23 -0300

    Created ORM Base

--- db.py
+++ db.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine#, Table, Column, String, Integer, select, text, MetaData
 from utils.constants import DB_URL
 
+class Base(DeclarativeBase):
+    ...
+
 engine = create_engine(DB_URL, echo=False)
 Session = sessionmaker(bind=engine)
 
