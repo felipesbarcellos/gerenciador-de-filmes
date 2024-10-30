fedffb572fcab116f5299ec1949c325faf6696ba
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-10-30 02:57:41 -0300

    refactored view

--- views/view.py
+++ views/view.py
@@ -3,46 +3,42 @@ from tkinter import ttk
 from controllers.filmes_controller import ControllerFilmes
 
 #data
-nomes = ['Palo Alto', 'Os sem floresta', 'Your Name']
-assistido = ['25/09/2024', '03/10/2024', '25/07/2024']
 class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
         self.filmes = []
-        self.create_table(self)
-        self.controller = ControllerFilmes(self)
-        self.controller.atualizar_lista_filmes()
+        self.controller = ControllerFilmes()
 
-    #Create table
-    def create_table(self, frame):
-        def delete_item(_):
-            for i in self.table.selection():
-                self.table.delete(i)
+        self.frame = ttk.Frame(self)
+        self.frame.pack()
 
-        def item_select(_):
-            for i in self.table.selection():
-                print(self.table.item(i)['values'])
-
-        self.table = ttk.Treeview(frame, columns=('nome', 'assistido_em'), show='headings')
+        self.table = ttk.Treeview(self.frame, columns=('nome', 'assistido_em'), show='headings')
+        self.table.grid(row=0, column=0)
 
         self.table.heading('nome', text = 'Nome')
         self.table.heading('assistido_em', text='Assistido em')
-        self.table.bind('<<TreeviewSelect>>', item_select)
-        self.table.bind('<Delete>', delete_item)
-        return self.table.pack(fill = 'both', expand=True)
+        self.table.bind('<<TreeviewSelect>>', self.item_select)
+        self.table.bind('<Delete>', self.delete_item)
+        self.atualizar_lista()
 
+    #Create table
+    def delete_item(self, _):
+        for i in self.table.selection():
+            self.table.delete(i)
 
+    def item_select(self, _):
+        for i in self.table.selection():
+            print(self.table.item(i)['values'])
 
-
-
-
-    def set_filmes(self, filmes):
+    def _set_filmes(self, filmes):
         self.filmes = filmes
 
-    def atualizar_lista(self, filmes):
-        # for record in self.table.get_children():
-        #     self.table.delete(record)
+    def atualizar_lista(self):
+        self._set_filmes(self.controller.get_filmes())
         
-        for filme in filmes:
+        for record in self.table.get_children():
+            self.table.delete(record)
+        
+        for filme in self.filmes:
             self.table.insert(parent='', index=0, values=(filme.nome, filme.assistido_em))
