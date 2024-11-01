8412322e73e37970527bfd7198136070e22b18d3
Felipe Barcellos <felipe.cellos@hotmail.com>
2024-11-01 17:48:55 -0300

    refactored to use sqlalchemy

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine#, Table, Column, String, Integer, select, text, MetaData
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 from utils.constants import DB_URL
@@ -5,8 +6,9 @@ from utils.constants import DB_URL
 class Base(DeclarativeBase):
     ...
 
-engine = create_engine(DB_URL, echo=False)
-Session = sessionmaker(bind=engine)
+# engine = create_engine(DB_URL, echo=False)
+db = SQLAlchemy(model_class=Base)
+Session = sessionmaker(bind=db)
 
 def cria_db():
-    Base.metadata.create_all(bind=engine)
+    Base.metadata.create_all(bind=db)
